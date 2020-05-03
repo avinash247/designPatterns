fb9a2f6a97ac3f446eaec031c71eda18430dd21b
avinash247 <avinash.hedaoo24@gmail.com>
2020-05-03 12:01:28 +0530

    Added demo solution for singleton pattern

---
+++ Singleton/Simple/LoggerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
